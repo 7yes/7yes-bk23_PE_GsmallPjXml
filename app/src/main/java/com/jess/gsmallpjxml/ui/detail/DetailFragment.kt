e30dfac82fe912f7a0ac990ce1cbba7d07bbc403
7yes <100316346+7yes@users.noreply.github.com>
2023-09-18 17:06:49 -0500

    navigate with args to details

--- app/src/main/java/com/jess/gsmallpjxml/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/jess/gsmallpjxml/ui/detail/DetailFragment.kt
@@ -1,12 +1,12 @@
 package com.jess.gsmallpjxml.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.jess.gsmallpjxml.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.jess.gsmallpjxml.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,15 +15,20 @@ class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
+    private val args: DetailFragmentArgs by navArgs()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
-        binding.tvDetail.setOnClickListener { findNavController().navigate(R.id.action_detailFragment_to_extraFragment) }
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(requireContext(), "es ${args.name}", Toast.LENGTH_SHORT).show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
