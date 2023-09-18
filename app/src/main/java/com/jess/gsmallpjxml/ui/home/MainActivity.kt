2cc395086fd6edb8ae6640a9e21e515909c88d45
7yes <100316346+7yes@users.noreply.github.com>
2023-09-18 14:19:57 -0500

    set up botton nav P1

--- app/src/main/java/com/jess/gsmallpjxml/ui/home/MainActivity.kt
+++ app/src/main/java/com/jess/gsmallpjxml/ui/home/MainActivity.kt
@@ -2,6 +2,9 @@ package com.jess.gsmallpjxml.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.jess.gsmallpjxml.R
 import com.jess.gsmallpjxml.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,10 +12,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        setContentView ( binding.root )
+        initBottomNavigation()
+
+        setContentView(binding.root)
+    }
+
+    private fun initBottomNavigation() {
+        val navHost: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.frag_container) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
